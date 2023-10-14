76b48341c8f0507072168023008efe7e0c3758ce
boskoa <bosko.ap@gmail.com>
2023-10-14 12:43:13 +0200

    Adding lint and testing actions

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, vi: true, "cypress/globals": true },
+  env: { browser: true, es2020: true, "cypress/globals": true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh", "cypress"],
